a051715ec247c31d77ddaef4a368db3174158444
szyryjn911 <amarkumarray47@gmail.com>
2026-06-15 20:02:55 +0530

    Added susfs support manually in cmdline.c

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -5,6 +5,11 @@
 #include <linux/seq_file.h>
 #include <asm/setup.h>
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern struct static_key_false susfs_is_fake_cmdline_or_bootconfig_buffer_set;
+extern void susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
 #if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG) || \
     defined(CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT)
 #define INITRAMFS_STR_FIND "skip_initramf"
@@ -47,6 +52,12 @@ static void proc_command_line_init(void) {
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (static_branch_likely(&susfs_is_fake_cmdline_or_bootconfig_buffer_set)) {
+		susfs_spoof_cmdline_or_bootconfig(m);
+		return 0;
+	}
+#endif
 	seq_printf(m, "%s\n", proc_command_line);
 	return 0;
 }
